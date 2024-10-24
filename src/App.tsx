6e1c485818e902de78a426cd0e8d15d111625719
Dogukan Kaan Bozkurt <dkaanbozkurt@gmail.com>
2024-10-24 10:50:13 +0200

    Prototype bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Hello from './components/Hello'
+import Background from './components/Background.tsx'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <Hello name="Developer" />
-    </div>
+
+    <Background />
+
   )
 }
 
